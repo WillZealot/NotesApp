156195d29ba182b553049999a686b7c5b6eadaa2
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-06-30 21:54:42 -0400

    finally got it to route to notes page the a tag in index.html wasnt set up correctly fixed that little issue

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'))
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
 app.get('/api/notes', (req,res) => {
@@ -28,4 +28,4 @@ app.post('/notes', (req,res) => {
     // give each note a unique id when it's saved (look into npm packages that could do this for you).
 });
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT} 🚀`));
