5e407240c48ad0d56090a3c10801e76b46c293ca
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-03 20:02:52 -0400

    added a post and a get route with an ws write file included with errors based on the responses

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
-const notes = require('./db/db.json');
 
 const app = express();
 app.use(express.json());
@@ -15,23 +14,45 @@ app.get('/notes', (req,res) => {
 });
 
 app.get('/api/notes', (req,res) => {
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-              // Handle the error
-              console.error(err);
-              return;
-            }
-          
-            // Process the file data
-            console.log(data);
-          });      
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          // Handle the error
+          console.error(err);
+          return res.status(500).json({ error: 'Failed to read notes from the database.' });
+        }
+    
+        try {
+          // Process the file data
+          const notes = JSON.parse(data);
+          res.json(notes);
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Failed to parse notes data.' });
+        }
+      });   
 });
 
 app.post('/notes', (req,res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          // Handle the error
+          console.error(err);
+          return res.status(500).json({ error: 'Failed to read notes from the database.' });
+        }
+    
+        try {
+          // Process the file data
+          const notes = JSON.parse(data);
+          res.json(notes);
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Failed to parse notes data.' });
+        }
+      });
+});
     //todo should receive a new note to save on the request body, add it to the
     // `db.json` file, and then return the new note to the client. You'll need to find a way to 
-    // give each note a unique id when it's saved (look into npm packages that could do this for you).
-});
+    // give each note a unique id when it's saved (look into npm packages that could do this for you)
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
